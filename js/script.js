d9da5b76a630c8402274ce5741fbfd3b18c97b87
heejung-newheee <aldls2311@gmail.com>
2023-06-01 02:12:04 +0900

    [수정]검색결과 없을때,빈칸삭제

--- js/script.js
+++ js/script.js
@@ -28,7 +28,7 @@ function listing(){
             let poster = a['poster_path'];
             let vote_avg = a['vote_average'];
             let _id = a['id'];
-            temp_html += `<div class="card-list" id="${_id}">
+            temp_html += `<div class="card-list" id="${_id}" onclick="alertId(${_id})">
                                 <img src="https://image.tmdb.org/t/p/original${poster}" class="card-img" />
                                 <div class="card-info">
                                     <h5 class="card-title">${title}</h5>
@@ -40,24 +40,25 @@ function listing(){
         });
 
         // 카드 클릭시 아이디 alert
-        cardWrap.addEventListener("click", function({target}){
-            // console.log("alert ID")
-            if (target !== cardWrap) {
-                if (target.className === "card-list") {
-                  alert(`영화 id: ${target._id}`);
-                } else {
-                  alert(`영화 id: ${target.parentNode.id}`);
-                }
-                location.reload();
-            }
-        });
-        
-        // 검색 키워드. filter / includes
+        // cardWrap.addEventListener("click", function({target}){
+        //     // console.log("alert ID")
+        //     if (target !== cardWrap) {
+        //         if (target.className === "card-list") {
+        //           alert(`영화 id: ${target._id}`);
+        //         } else {
+        //           alert(`영화 id: ${target.parentNode.id}`);
+        //         }
+        //     }
+        // });
+
+
+        // 검색 키워드 includes
         let searchBtn = document.getElementById("search-btn");
         searchBtn.addEventListener("click", function(){
             let temp_html = '';  
+            // input 입력값
             let inputValue =  document.getElementById("search-input").value.toLowerCase();
-            // console.log(inputValue);
+            console.log('입력값->',inputValue);
 
             movies.forEach((a)=>{
                 let title = a['original_title'];
@@ -66,11 +67,11 @@ function listing(){
                 let vote_avg = a['vote_average'];
                 let _id = a['id'];
                 
-                // 대소문자 관계없이 검색 가능
-                let titleLower = title.toLowerCase();
-
+                // 대소문자, 띄어쓰기 관계없이 검색 가능
+                inputValue = inputValue.split(' ').join('');
+                let titleLower = title.split(' ').join('').toLowerCase();
                 if(titleLower.includes(inputValue)){
-                    temp_html +=`<div class="card-list" id="${_id}">
+                    temp_html +=`<div class="card-list" id="${_id}" onclick="alertId(${_id})">
                                     <img src="https://image.tmdb.org/t/p/original${poster}" class="card-img" />
                                     <div class="card-info">
                                         <h5 class="card-title">${title}</h5>
@@ -79,16 +80,15 @@ function listing(){
                                     </div>
                                 </div>`       
                     cardWrap.innerHTML = temp_html;
-                    console.log("영화잇음");
-                    // let input = .value = null ; 인풋창 지우기
-
-                } else {
-                    // cardWrap.innerHTML = `검색한 영화가 없습니다. 다시 검색해 주세요.`
-                    // // alert("검색한 영화가 없습니다! 다시 검색해 주세요.");
-                    // // window.location.reload();
-                    // window.location.href='index.html';
-                }
+                    console.log("영화있음");
+                } 
             });
+
+            // 검색한 영화가 없을때
+            if(document.querySelectorAll(".card-list").length === 20 ){
+                alert("검색된 영화가 없습니다! 다시 검색해 주세요.");
+                window.location.reload();
+            }
         });
 
         // 키보드 enter키 입력
@@ -101,6 +101,12 @@ function listing(){
     })
     .catch((err) => console.error(err));
 }
+
+// 카드 클릭 시 영화 ID alert
+let alertId = function (_id) {
+    alert(`영화 id : ${_id}`);
+};
+
 
 // 영화 검색 결과 없을 때.
 // 검색창 리로드(비워주기)
